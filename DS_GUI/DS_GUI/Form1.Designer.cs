2d4e41465546aabcf9eaf1da025428c7f69f118e
Jonard <ji.kok@student.han.nl>
2022-10-30 18:32:17 +0100

    invoert student gemaakt totaan studierichting

--- DS_GUI/DS_GUI/Form1.Designer.cs
+++ DS_GUI/DS_GUI/Form1.Designer.cs
@@ -28,20 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.naamtextbox = new System.Windows.Forms.TextBox();
+            this.naamlabel = new System.Windows.Forms.Label();
+            this.invoerstudentlabel = new System.Windows.Forms.Label();
+            this.invoerachtergrond = new System.Windows.Forms.PictureBox();
+            this.voornaamlabel = new System.Windows.Forms.Label();
+            this.manvrouwlabel = new System.Windows.Forms.Label();
+            this.adreslabel = new System.Windows.Forms.Label();
+            this.woonplaatslabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.studierichtinglabel = new System.Windows.Forms.Label();
+            this.studierichtingtextbox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.invoerachtergrond)).BeginInit();
             this.SuspendLayout();
+            // 
+            // naamtextbox
+            // 
+            this.naamtextbox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.naamtextbox.Location = new System.Drawing.Point(202, 97);
+            this.naamtextbox.Name = "naamtextbox";
+            this.naamtextbox.Size = new System.Drawing.Size(243, 30);
+            this.naamtextbox.TabIndex = 0;
+            // 
+            // naamlabel
+            // 
+            this.naamlabel.AutoSize = true;
+            this.naamlabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.naamlabel.Location = new System.Drawing.Point(40, 100);
+            this.naamlabel.Name = "naamlabel";
+            this.naamlabel.Size = new System.Drawing.Size(64, 23);
+            this.naamlabel.TabIndex = 1;
+            this.naamlabel.Text = "Naam";
+            // 
+            // invoerstudentlabel
+            // 
+            this.invoerstudentlabel.AutoSize = true;
+            this.invoerstudentlabel.BackColor = System.Drawing.Color.Transparent;
+            this.invoerstudentlabel.Font = new System.Drawing.Font("Verdana", 20F);
+            this.invoerstudentlabel.Location = new System.Drawing.Point(25, 25);
+            this.invoerstudentlabel.Name = "invoerstudentlabel";
+            this.invoerstudentlabel.Size = new System.Drawing.Size(328, 32);
+            this.invoerstudentlabel.TabIndex = 2;
+            this.invoerstudentlabel.Text = "Invoer Nieuwe Student";
+            // 
+            // invoerachtergrond
+            // 
+            this.invoerachtergrond.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.invoerachtergrond.Location = new System.Drawing.Point(12, 12);
+            this.invoerachtergrond.Name = "invoerachtergrond";
+            this.invoerachtergrond.Size = new System.Drawing.Size(600, 648);
+            this.invoerachtergrond.TabIndex = 3;
+            this.invoerachtergrond.TabStop = false;
+            // 
+            // voornaamlabel
+            // 
+            this.voornaamlabel.AutoSize = true;
+            this.voornaamlabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.voornaamlabel.Location = new System.Drawing.Point(40, 150);
+            this.voornaamlabel.Name = "voornaamlabel";
+            this.voornaamlabel.Size = new System.Drawing.Size(106, 23);
+            this.voornaamlabel.TabIndex = 4;
+            this.voornaamlabel.Text = "Voornaam";
+            // 
+            // manvrouwlabel
+            // 
+            this.manvrouwlabel.AutoSize = true;
+            this.manvrouwlabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.manvrouwlabel.Location = new System.Drawing.Point(40, 300);
+            this.manvrouwlabel.Name = "manvrouwlabel";
+            this.manvrouwlabel.Size = new System.Drawing.Size(119, 23);
+            this.manvrouwlabel.TabIndex = 5;
+            this.manvrouwlabel.Text = "Man/Vrouw";
+            // 
+            // adreslabel
+            // 
+            this.adreslabel.AutoSize = true;
+            this.adreslabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.adreslabel.Location = new System.Drawing.Point(40, 250);
+            this.adreslabel.Name = "adreslabel";
+            this.adreslabel.Size = new System.Drawing.Size(64, 23);
+            this.adreslabel.TabIndex = 6;
+            this.adreslabel.Text = "Adres";
+            // 
+            // woonplaatslabel
+            // 
+            this.woonplaatslabel.AutoSize = true;
+            this.woonplaatslabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.woonplaatslabel.Location = new System.Drawing.Point(40, 200);
+            this.woonplaatslabel.Name = "woonplaatslabel";
+            this.woonplaatslabel.Size = new System.Drawing.Size(121, 23);
+            this.woonplaatslabel.TabIndex = 7;
+            this.woonplaatslabel.Text = "Woonplaats";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label5.Location = new System.Drawing.Point(40, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "onbekendpaslabelaan";
+            // 
+            // studierichtinglabel
+            // 
+            this.studierichtinglabel.AutoSize = true;
+            this.studierichtinglabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.studierichtinglabel.Location = new System.Drawing.Point(40, 400);
+            this.studierichtinglabel.Name = "studierichtinglabel";
+            this.studierichtinglabel.Size = new System.Drawing.Size(156, 23);
+            this.studierichtinglabel.TabIndex = 9;
+            this.studierichtinglabel.Text = "Studie Richting";
+            // 
+            // studierichtingtextbox
+            // 
+            this.studierichtingtextbox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.studierichtingtextbox.Location = new System.Drawing.Point(202, 397);
+            this.studierichtingtextbox.Name = "studierichtingtextbox";
+            this.studierichtingtextbox.Size = new System.Drawing.Size(243, 30);
+            this.studierichtingtextbox.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 14F);
+            this.textBox1.Location = new System.Drawing.Point(202, 297);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 30);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.textBox2.Location = new System.Drawing.Point(202, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 30);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Verdana", 14F);
+            this.textBox3.Location = new System.Drawing.Point(202, 197);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 30);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Verdana", 14F);
+            this.textBox4.Location = new System.Drawing.Point(202, 147);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(243, 30);
+            this.textBox4.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.studierichtingtextbox);
+            this.Controls.Add(this.studierichtinglabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.woonplaatslabel);
+            this.Controls.Add(this.adreslabel);
+            this.Controls.Add(this.manvrouwlabel);
+            this.Controls.Add(this.voornaamlabel);
+            this.Controls.Add(this.invoerstudentlabel);
+            this.Controls.Add(this.naamlabel);
+            this.Controls.Add(this.naamtextbox);
+            this.Controls.Add(this.invoerachtergrond);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.invoerachtergrond)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox naamtextbox;
+        private System.Windows.Forms.Label naamlabel;
+        private System.Windows.Forms.Label invoerstudentlabel;
+        private System.Windows.Forms.PictureBox invoerachtergrond;
+        private System.Windows.Forms.Label voornaamlabel;
+        private System.Windows.Forms.Label manvrouwlabel;
+        private System.Windows.Forms.Label adreslabel;
+        private System.Windows.Forms.Label woonplaatslabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label studierichtinglabel;
+        private System.Windows.Forms.TextBox studierichtingtextbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- DS_GUI/DS_GUI/Form1.cs
+++ DS_GUI/DS_GUI/Form1.cs
@@ -16,5 +16,10 @@ namespace DS_GUI
         {
             InitializeComponent();
         }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
