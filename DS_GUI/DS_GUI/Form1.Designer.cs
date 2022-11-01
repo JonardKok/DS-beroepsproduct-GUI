09c3f2a5364a73b689b05942006aa552362a60a6
Jonard <ji.kok@student.han.nl>
2022-11-01 16:59:58 +0100

    Ophalen user klaar, maken user -groep klaar

--- DS_GUI/DS_GUI/Form1.Designer.cs
+++ DS_GUI/DS_GUI/Form1.Designer.cs
@@ -37,14 +37,13 @@
             this.invoermanvrouwlabel = new System.Windows.Forms.Label();
             this.invoeradreslabel = new System.Windows.Forms.Label();
             this.invoerwoonplaatslabel = new System.Windows.Forms.Label();
-            this.invoeremailadreslabel = new System.Windows.Forms.Label();
+            this.invoerwachtwoordlabel = new System.Windows.Forms.Label();
             this.invoerstudierichtinglabel = new System.Windows.Forms.Label();
-            this.invoerstudierichtingtextbox = new System.Windows.Forms.TextBox();
             this.invoermanvrouwtextbox = new System.Windows.Forms.TextBox();
             this.invoeradrestextbox = new System.Windows.Forms.TextBox();
             this.invoerwoonplaatstextbox = new System.Windows.Forms.TextBox();
             this.invoervoornaamtextbox = new System.Windows.Forms.TextBox();
-            this.invoeremailadrestextbox = new System.Windows.Forms.TextBox();
+            this.invoerwachtwoordtextbox = new System.Windows.Forms.TextBox();
             this.invoerinlognaambutton = new System.Windows.Forms.Button();
             this.invoerinlognaamtextbox = new System.Windows.Forms.TextBox();
             this.invoerinlognaamlabel = new System.Windows.Forms.Label();
@@ -60,10 +59,9 @@
             this.wijzigenadreslabel = new System.Windows.Forms.Label();
             this.wijzigenmanvrouwlabel = new System.Windows.Forms.Label();
             this.wijzigengeboortedatumlabel = new System.Windows.Forms.Label();
-            this.wijzigenemailadreslabel = new System.Windows.Forms.Label();
+            this.wijzigewachtwoordlabel = new System.Windows.Forms.Label();
             this.wijzigenstudierichtinglabel = new System.Windows.Forms.Label();
-            this.wijzigenemailadrestextbox = new System.Windows.Forms.TextBox();
-            this.wijzigenstudierichtingtextbox = new System.Windows.Forms.TextBox();
+            this.wijzigenwachtwoordtextbox = new System.Windows.Forms.TextBox();
             this.wijzigenmanvrouwtextbox = new System.Windows.Forms.TextBox();
             this.wijzigenadrestextbox = new System.Windows.Forms.TextBox();
             this.wijzigenwoonplaatstextbox = new System.Windows.Forms.TextBox();
@@ -74,6 +72,9 @@
             this.wijzigenstudentlabel = new System.Windows.Forms.Label();
             this.invoergeboortedatummaskedtextbox = new System.Windows.Forms.MaskedTextBox();
             this.wijzigengeboortedatummaskedtextbox = new System.Windows.Forms.MaskedTextBox();
+            this.invoerwachtwoordlatenzienbutton = new System.Windows.Forms.Button();
+            this.invoerstudierichtingcombobox = new System.Windows.Forms.ComboBox();
+            this.wijzigenstudierichtingcombobox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.invoerachtergrond)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wijzigenachtergrond)).BeginInit();
             this.SuspendLayout();
@@ -156,15 +157,15 @@
             this.invoerwoonplaatslabel.TabIndex = 7;
             this.invoerwoonplaatslabel.Text = "Woonplaats";
             // 
-            // invoeremailadreslabel
+            // invoerwachtwoordlabel
             // 
-            this.invoeremailadreslabel.AutoSize = true;
-            this.invoeremailadreslabel.Font = new System.Drawing.Font("Verdana", 14F);
-            this.invoeremailadreslabel.Location = new System.Drawing.Point(25, 400);
-            this.invoeremailadreslabel.Name = "invoeremailadreslabel";
-            this.invoeremailadreslabel.Size = new System.Drawing.Size(133, 23);
-            this.invoeremailadreslabel.TabIndex = 8;
-            this.invoeremailadreslabel.Text = "E-mail Adres";
+            this.invoerwachtwoordlabel.AutoSize = true;
+            this.invoerwachtwoordlabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.invoerwachtwoordlabel.Location = new System.Drawing.Point(25, 400);
+            this.invoerwachtwoordlabel.Name = "invoerwachtwoordlabel";
+            this.invoerwachtwoordlabel.Size = new System.Drawing.Size(128, 23);
+            this.invoerwachtwoordlabel.TabIndex = 8;
+            this.invoerwachtwoordlabel.Text = "Wachtwoord";
             // 
             // invoerstudierichtinglabel
             // 
@@ -175,14 +176,6 @@
             this.invoerstudierichtinglabel.Size = new System.Drawing.Size(156, 23);
             this.invoerstudierichtinglabel.TabIndex = 9;
             this.invoerstudierichtinglabel.Text = "Studie Richting";
-            // 
-            // invoerstudierichtingtextbox
-            // 
-            this.invoerstudierichtingtextbox.Font = new System.Drawing.Font("Verdana", 14F);
-            this.invoerstudierichtingtextbox.Location = new System.Drawing.Point(189, 447);
-            this.invoerstudierichtingtextbox.Name = "invoerstudierichtingtextbox";
-            this.invoerstudierichtingtextbox.Size = new System.Drawing.Size(230, 30);
-            this.invoerstudierichtingtextbox.TabIndex = 9;
             // 
             // invoermanvrouwtextbox
             // 
@@ -216,29 +209,30 @@
             this.invoervoornaamtextbox.Size = new System.Drawing.Size(230, 30);
             this.invoervoornaamtextbox.TabIndex = 3;
             // 
-            // invoeremailadrestextbox
+            // invoerwachtwoordtextbox
             // 
-            this.invoeremailadrestextbox.Font = new System.Drawing.Font("Verdana", 14F);
-            this.invoeremailadrestextbox.Location = new System.Drawing.Point(189, 397);
-            this.invoeremailadrestextbox.Name = "invoeremailadrestextbox";
-            this.invoeremailadrestextbox.Size = new System.Drawing.Size(230, 30);
-            this.invoeremailadrestextbox.TabIndex = 8;
+            this.invoerwachtwoordtextbox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.invoerwachtwoordtextbox.Location = new System.Drawing.Point(189, 397);
+            this.invoerwachtwoordtextbox.Name = "invoerwachtwoordtextbox";
+            this.invoerwachtwoordtextbox.PasswordChar = '*';
+            this.invoerwachtwoordtextbox.Size = new System.Drawing.Size(230, 30);
+            this.invoerwachtwoordtextbox.TabIndex = 8;
             // 
             // invoerinlognaambutton
             // 
             this.invoerinlognaambutton.Font = new System.Drawing.Font("Verdana", 10F);
-            this.invoerinlognaambutton.Location = new System.Drawing.Point(432, 93);
+            this.invoerinlognaambutton.Location = new System.Drawing.Point(425, 96);
             this.invoerinlognaambutton.Name = "invoerinlognaambutton";
-            this.invoerinlognaambutton.Size = new System.Drawing.Size(175, 30);
+            this.invoerinlognaambutton.Size = new System.Drawing.Size(175, 45);
             this.invoerinlognaambutton.TabIndex = 1;
-            this.invoerinlognaambutton.Text = "Genereer Inlognaam";
+            this.invoerinlognaambutton.Text = "Genereer Inloggegevens";
             this.invoerinlognaambutton.UseVisualStyleBackColor = true;
             this.invoerinlognaambutton.Click += new System.EventHandler(this.invoerinlognaambutton_Click);
             // 
             // invoerinlognaamtextbox
             // 
             this.invoerinlognaamtextbox.Font = new System.Drawing.Font("Verdana", 14F);
-            this.invoerinlognaamtextbox.Location = new System.Drawing.Point(443, 129);
+            this.invoerinlognaamtextbox.Location = new System.Drawing.Point(443, 147);
             this.invoerinlognaamtextbox.Name = "invoerinlognaamtextbox";
             this.invoerinlognaamtextbox.Size = new System.Drawing.Size(150, 30);
             this.invoerinlognaamtextbox.TabIndex = 2;
@@ -247,7 +241,7 @@
             // 
             this.invoerinlognaamlabel.AutoSize = true;
             this.invoerinlognaamlabel.Font = new System.Drawing.Font("Verdana", 14F);
-            this.invoerinlognaamlabel.Location = new System.Drawing.Point(460, 162);
+            this.invoerinlognaamlabel.Location = new System.Drawing.Point(454, 180);
             this.invoerinlognaamlabel.Name = "invoerinlognaamlabel";
             this.invoerinlognaamlabel.Size = new System.Drawing.Size(112, 23);
             this.invoerinlognaamlabel.TabIndex = 12;
@@ -292,6 +286,7 @@
             this.wijzigeninlognaambutton.TabIndex = 15;
             this.wijzigeninlognaambutton.Text = "Haal Student Op";
             this.wijzigeninlognaambutton.UseVisualStyleBackColor = true;
+            this.wijzigeninlognaambutton.Click += new System.EventHandler(this.wijzigeninlognaambutton_Click);
             // 
             // wijzigeninlognaamtextbox
             // 
@@ -371,15 +366,15 @@
             this.wijzigengeboortedatumlabel.TabIndex = 23;
             this.wijzigengeboortedatumlabel.Text = "Geboortedatum";
             // 
-            // wijzigenemailadreslabel
+            // wijzigewachtwoordlabel
             // 
-            this.wijzigenemailadreslabel.AutoSize = true;
-            this.wijzigenemailadreslabel.Font = new System.Drawing.Font("Verdana", 14F);
-            this.wijzigenemailadreslabel.Location = new System.Drawing.Point(845, 400);
-            this.wijzigenemailadreslabel.Name = "wijzigenemailadreslabel";
-            this.wijzigenemailadreslabel.Size = new System.Drawing.Size(133, 23);
-            this.wijzigenemailadreslabel.TabIndex = 24;
-            this.wijzigenemailadreslabel.Text = "E-mail Adres";
+            this.wijzigewachtwoordlabel.AutoSize = true;
+            this.wijzigewachtwoordlabel.Font = new System.Drawing.Font("Verdana", 14F);
+            this.wijzigewachtwoordlabel.Location = new System.Drawing.Point(845, 400);
+            this.wijzigewachtwoordlabel.Name = "wijzigewachtwoordlabel";
+            this.wijzigewachtwoordlabel.Size = new System.Drawing.Size(128, 23);
+            this.wijzigewachtwoordlabel.TabIndex = 24;
+            this.wijzigewachtwoordlabel.Text = "Wachtwoord";
             // 
             // wijzigenstudierichtinglabel
             // 
@@ -391,21 +386,13 @@
             this.wijzigenstudierichtinglabel.TabIndex = 25;
             this.wijzigenstudierichtinglabel.Text = "Studie Richting";
             // 
-            // wijzigenemailadrestextbox
+            // wijzigenwachtwoordtextbox
             // 
-            this.wijzigenemailadrestextbox.Font = new System.Drawing.Font("Verdana", 14F);
-            this.wijzigenemailadrestextbox.Location = new System.Drawing.Point(1009, 397);
-            this.wijzigenemailadrestextbox.Name = "wijzigenemailadrestextbox";
-            this.wijzigenemailadrestextbox.Size = new System.Drawing.Size(230, 30);
-            this.wijzigenemailadrestextbox.TabIndex = 27;
-            // 
-            // wijzigenstudierichtingtextbox
-            // 
-            this.wijzigenstudierichtingtextbox.Font = new System.Drawing.Font("Verdana", 14F);
-            this.wijzigenstudierichtingtextbox.Location = new System.Drawing.Point(1009, 447);
-            this.wijzigenstudierichtingtextbox.Name = "wijzigenstudierichtingtextbox";
-            this.wijzigenstudierichtingtextbox.Size = new System.Drawing.Size(230, 30);
-            this.wijzigenstudierichtingtextbox.TabIndex = 28;
+            this.wijzigenwachtwoordtextbox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.wijzigenwachtwoordtextbox.Location = new System.Drawing.Point(1009, 397);
+            this.wijzigenwachtwoordtextbox.Name = "wijzigenwachtwoordtextbox";
+            this.wijzigenwachtwoordtextbox.Size = new System.Drawing.Size(230, 30);
+            this.wijzigenwachtwoordtextbox.TabIndex = 27;
             // 
             // wijzigenmanvrouwtextbox
             // 
@@ -496,11 +483,52 @@
             this.wijzigengeboortedatummaskedtextbox.Size = new System.Drawing.Size(230, 30);
             this.wijzigengeboortedatummaskedtextbox.TabIndex = 38;
             // 
+            // invoerwachtwoordlatenzienbutton
+            // 
+            this.invoerwachtwoordlatenzienbutton.Location = new System.Drawing.Point(425, 400);
+            this.invoerwachtwoordlatenzienbutton.Name = "invoerwachtwoordlatenzienbutton";
+            this.invoerwachtwoordlatenzienbutton.Size = new System.Drawing.Size(129, 23);
+            this.invoerwachtwoordlatenzienbutton.TabIndex = 39;
+            this.invoerwachtwoordlatenzienbutton.Text = "Wachtwoord laten zien";
+            this.invoerwachtwoordlatenzienbutton.UseVisualStyleBackColor = true;
+            this.invoerwachtwoordlatenzienbutton.Click += new System.EventHandler(this.invoerwachtwoordlatenzienbutton_Click);
+            // 
+            // invoerstudierichtingcombobox
+            // 
+            this.invoerstudierichtingcombobox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.invoerstudierichtingcombobox.FormattingEnabled = true;
+            this.invoerstudierichtingcombobox.Items.AddRange(new object[] {
+            "APPDEV",
+            "BIM",
+            "ISM",
+            "WEBDEV"});
+            this.invoerstudierichtingcombobox.Location = new System.Drawing.Point(189, 447);
+            this.invoerstudierichtingcombobox.Name = "invoerstudierichtingcombobox";
+            this.invoerstudierichtingcombobox.Size = new System.Drawing.Size(230, 31);
+            this.invoerstudierichtingcombobox.TabIndex = 40;
+            // 
+            // wijzigenstudierichtingcombobox
+            // 
+            this.wijzigenstudierichtingcombobox.Font = new System.Drawing.Font("Verdana", 14F);
+            this.wijzigenstudierichtingcombobox.FormattingEnabled = true;
+            this.wijzigenstudierichtingcombobox.Items.AddRange(new object[] {
+            "APPDEV",
+            "BIM",
+            "ISM",
+            "WEBDEV"});
+            this.wijzigenstudierichtingcombobox.Location = new System.Drawing.Point(1009, 447);
+            this.wijzigenstudierichtingcombobox.Name = "wijzigenstudierichtingcombobox";
+            this.wijzigenstudierichtingcombobox.Size = new System.Drawing.Size(230, 31);
+            this.wijzigenstudierichtingcombobox.TabIndex = 41;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 561);
+            this.Controls.Add(this.wijzigenstudierichtingcombobox);
+            this.Controls.Add(this.invoerstudierichtingcombobox);
+            this.Controls.Add(this.invoerwachtwoordlatenzienbutton);
             this.Controls.Add(this.wijzigengeboortedatummaskedtextbox);
             this.Controls.Add(this.invoergeboortedatummaskedtextbox);
             this.Controls.Add(this.wijzigenstudentlabel);
@@ -511,10 +539,9 @@
             this.Controls.Add(this.wijzigenwoonplaatstextbox);
             this.Controls.Add(this.wijzigenadrestextbox);
             this.Controls.Add(this.wijzigenmanvrouwtextbox);
-            this.Controls.Add(this.wijzigenstudierichtingtextbox);
-            this.Controls.Add(this.wijzigenemailadrestextbox);
+            this.Controls.Add(this.wijzigenwachtwoordtextbox);
             this.Controls.Add(this.wijzigenstudierichtinglabel);
-            this.Controls.Add(this.wijzigenemailadreslabel);
+            this.Controls.Add(this.wijzigewachtwoordlabel);
             this.Controls.Add(this.wijzigengeboortedatumlabel);
             this.Controls.Add(this.wijzigenmanvrouwlabel);
             this.Controls.Add(this.wijzigenadreslabel);
@@ -530,14 +557,13 @@
             this.Controls.Add(this.invoerinlognaamlabel);
             this.Controls.Add(this.invoerinlognaamtextbox);
             this.Controls.Add(this.invoerinlognaambutton);
-            this.Controls.Add(this.invoeremailadrestextbox);
+            this.Controls.Add(this.invoerwachtwoordtextbox);
             this.Controls.Add(this.invoervoornaamtextbox);
             this.Controls.Add(this.invoerwoonplaatstextbox);
             this.Controls.Add(this.invoeradrestextbox);
             this.Controls.Add(this.invoermanvrouwtextbox);
-            this.Controls.Add(this.invoerstudierichtingtextbox);
             this.Controls.Add(this.invoerstudierichtinglabel);
-            this.Controls.Add(this.invoeremailadreslabel);
+            this.Controls.Add(this.invoerwachtwoordlabel);
             this.Controls.Add(this.invoerwoonplaatslabel);
             this.Controls.Add(this.invoeradreslabel);
             this.Controls.Add(this.invoermanvrouwlabel);
@@ -570,14 +596,13 @@
         private System.Windows.Forms.Label invoermanvrouwlabel;
         private System.Windows.Forms.Label invoeradreslabel;
         private System.Windows.Forms.Label invoerwoonplaatslabel;
-        private System.Windows.Forms.Label invoeremailadreslabel;
+        private System.Windows.Forms.Label invoerwachtwoordlabel;
         private System.Windows.Forms.Label invoerstudierichtinglabel;
-        private System.Windows.Forms.TextBox invoerstudierichtingtextbox;
         private System.Windows.Forms.TextBox invoermanvrouwtextbox;
         private System.Windows.Forms.TextBox invoeradrestextbox;
         private System.Windows.Forms.TextBox invoerwoonplaatstextbox;
         private System.Windows.Forms.TextBox invoervoornaamtextbox;
-        private System.Windows.Forms.TextBox invoeremailadrestextbox;
+        private System.Windows.Forms.TextBox invoerwachtwoordtextbox;
         private System.Windows.Forms.Button invoerinlognaambutton;
         private System.Windows.Forms.TextBox invoerinlognaamtextbox;
         private System.Windows.Forms.Label invoerinlognaamlabel;
@@ -593,10 +618,9 @@
         private System.Windows.Forms.Label wijzigenadreslabel;
         private System.Windows.Forms.Label wijzigenmanvrouwlabel;
         private System.Windows.Forms.Label wijzigengeboortedatumlabel;
-        private System.Windows.Forms.Label wijzigenemailadreslabel;
+        private System.Windows.Forms.Label wijzigewachtwoordlabel;
         private System.Windows.Forms.Label wijzigenstudierichtinglabel;
-        private System.Windows.Forms.TextBox wijzigenemailadrestextbox;
-        private System.Windows.Forms.TextBox wijzigenstudierichtingtextbox;
+        private System.Windows.Forms.TextBox wijzigenwachtwoordtextbox;
         private System.Windows.Forms.TextBox wijzigenmanvrouwtextbox;
         private System.Windows.Forms.TextBox wijzigenadrestextbox;
         private System.Windows.Forms.TextBox wijzigenwoonplaatstextbox;
@@ -607,6 +631,9 @@
         private System.Windows.Forms.Label wijzigenstudentlabel;
         private System.Windows.Forms.MaskedTextBox invoergeboortedatummaskedtextbox;
         private System.Windows.Forms.MaskedTextBox wijzigengeboortedatummaskedtextbox;
+        private System.Windows.Forms.Button invoerwachtwoordlatenzienbutton;
+        private System.Windows.Forms.ComboBox invoerstudierichtingcombobox;
+        private System.Windows.Forms.ComboBox wijzigenstudierichtingcombobox;
     }
 }
 
--- DS_GUI/DS_GUI/Form1.cs
+++ DS_GUI/DS_GUI/Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.DirectoryServices; //Toegevoegd om LDAP verbinding mogelijk te maken.
+using System.Security;
+//Toegevoegd om LDAP verbinding mogelijk te maken.
+using System.DirectoryServices; 
+using System.DirectoryServices.AccountManagement;
 
 namespace DS_GUI
 {
@@ -18,11 +21,12 @@ namespace DS_GUI
             InitializeComponent();
         }
 
-        private void progressBar1_Click(object sender, EventArgs e)
+        private void progressBar1_Click(object sender, EventArgs e) //foutje 
         {
 
         }
         
+        //Genereer inlognaam
         private void invoerinlognaambutton_Click(object sender, EventArgs e)
         {
             //Kijk of er een nummer in de naam zit
@@ -38,7 +42,7 @@ namespace DS_GUI
             //Check of er wel een naam ingevuld is en of deze te lang is.
             if (invoernaamtextbox.Text.Length == 0)
             {
-                MessageBox.Show("Er is geen naam ingevuld, vul deze in om een Inlognaam te genereren.", "Invoerfout", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Er is geen naam ingevuld, vul deze in om een Inlognaam en wachtwoord te genereren.", "Invoerfout", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             } else if (invoernaamtextbox.Text.Length <= 5)
             {
@@ -51,38 +55,109 @@ namespace DS_GUI
             } 
             //Voeg 5 willekeurige nummers toe aan de gebruikersnaam.
             Random random = new Random();
-            for (int i = 0; i < 5; i++) {
-                Console.WriteLine(i.ToString());
+            for (int i = 0; i < 5; i++) 
+            {
                 int num = random.Next(1, 10);
                 invoerinlognaamtextbox.Text = invoerinlognaamtextbox.Text + num.ToString(); 
             }
+            //genereer wachtwoord
+            invoerwachtwoordtextbox.Text = ""; //Zorgt ervoor dat het wachtwoordvak leeg is.
+            string allowedPasswordCharacters = "aAbBcCdDeEfFgGhHiIjJhHkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ01234567890123456789,;:!*$@-_=,;:!*$@-_=";
+            int length = random.Next(8, 15);
+            for (int i = 0; i < length; i++)
+            {
+                int characterNumber = random.Next(1, allowedPasswordCharacters.Length);
+                invoerwachtwoordtextbox.Text = invoerwachtwoordtextbox.Text + allowedPasswordCharacters[characterNumber];
+            }
             return;
         }
+
+        //Opslaan user
         private void invoeropslaanbutton_Click(object sender, EventArgs e)
         {
-            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://jonard.prive", "administrator", "b00m!23");
-
-            // DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://example.com");
-            DirectorySearcher searcher = new DirectorySearcher(directoryEntry)
+            try
             {
-                PageSize = int.MaxValue,
-                Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=t2_albert))"
-            };
-
-            searcher.PropertiesToLoad.Add("sn");
-
-            var result = searcher.FindOne();
-
-            if (result == null)
+                DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://OU=" + invoerstudierichtingcombobox.Text + ",DC=jonard,DC=prive");
+                DirectoryEntry childEntry = directoryEntry.Children.Add("CN=" + invoerinlognaamtextbox.Text, "user");
+                childEntry.Properties["samAccountName"].Value = invoerinlognaamtextbox.Text; //Accountnaam
+                childEntry.Properties["sn"].Value = invoernaamtextbox.Text; //Achternaam
+                childEntry.Properties["givenName"].Value = invoervoornaamtextbox.Text; //Voornaam
+                childEntry.Properties["l"].Value = invoerwoonplaatstextbox.Text; //Woonplaats
+                childEntry.Properties["streetAddress"].Value = invoeradrestextbox.Text; //Adres
+                childEntry.Properties["personalTitle"].Value = invoergeboortedatummaskedtextbox.Text; //Geslacht
+                childEntry.Properties["info"].Value = invoervoornaamtextbox.Text; // Geboortedatum
+                childEntry.CommitChanges();
+                directoryEntry.CommitChanges();
+                childEntry.Invoke("SetPassword", new object[] { invoerwachtwoordtextbox });
+                childEntry.CommitChanges();
+                MessageBox.Show("Gebruiker toegevoegd", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex)
             {
-                return; // Or whatever you need to do in this case
+              MessageBox.Show(ex.ToString(), "FOUT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+        //Wachtwoord laten zien.
+        private void invoerwachtwoordlatenzienbutton_Click(object sender, EventArgs e)
+        {
+            //Vraagt voor of het wachtwoord getoond moet worden
+            if (invoerwachtwoordtextbox.PasswordChar == '*')
+            {
+                DialogResult result;
+                result = MessageBox.Show("Weet u zeker dat u het wachtwoord wil weergeven?", "Veiligheidsvraag", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    return;
+                }
+                invoerwachtwoordlatenzienbutton.Text = "Wachtwoord verbergen";
+                invoerwachtwoordtextbox.PasswordChar = '\0'; //Geeft een null character aan PasswordChar
+                return;
+            } else
+            {
+                invoerwachtwoordtextbox.PasswordChar = '*';
+                invoerwachtwoordlatenzienbutton.Text = "Wachtwoord laten zien";
+            }
+        }
+
+        //Ophalen gegevens
+        private void wijzigeninlognaambutton_Click(object sender, EventArgs e)
+        {
+            if (wijzigeninlognaamtextbox.Text == "" || wijzigeninlognaamtextbox.Text == " ")
+            {
+                MessageBox.Show("Er is geen naam ingevuld", "Invoerfout", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
 
-            string surname;
-
-            if (result.Properties.Contains("sn"))
+            int i = 0;
+            foreach (string OU in wijzigenstudierichtingcombobox.Items)
             {
-                surname = result.Properties["sn"][0].ToString();
+                DirectoryEntry adUserFolder = new DirectoryEntry("LDAP://OU=" + OU + ",DC=jonard,DC=prive");
+                DirectorySearcher searcher = new DirectorySearcher(adUserFolder)
+                {
+                    PageSize = int.MaxValue,
+                    Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=" + wijzigeninlognaamtextbox.Text + "))"
+                };
+                var gebruikerInfo = searcher.FindOne();
+                if (gebruikerInfo == null)
+                {
+                    if (i == wijzigenstudierichtingcombobox.Items.Count - 1)
+                    {
+                        MessageBox.Show("Er is geen gebruiker met die inlognaam", "Melding", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    i++;
+                    continue;
+                }
+                
+                wijzigennaamtextbox.Text =  gebruikerInfo.Properties.Contains("sn") ? gebruikerInfo.Properties["sn"][0].ToString() : "";
+                wijzigenvoornaamtextbox.Text = gebruikerInfo.Properties.Contains("givenName") ? gebruikerInfo.Properties["sn"][0].ToString() : "";
+                wijzigenwoonplaatstextbox.Text = gebruikerInfo.Properties.Contains("l") ? gebruikerInfo.Properties["l"][0].ToString() : "";
+                wijzigenadrestextbox.Text = gebruikerInfo.Properties.Contains("streetAddress") ? gebruikerInfo.Properties["streetAddress"][0].ToString() : "";
+                wijzigenmanvrouwtextbox.Text = gebruikerInfo.Properties.Contains("personalTitle") ? gebruikerInfo.Properties["personalTitle"][0].ToString() : "";
+                wijzigengeboortedatummaskedtextbox.Text = gebruikerInfo.Properties.Contains("info") ? gebruikerInfo.Properties["info"][0].ToString() : "";
+                wijzigenstudierichtingcombobox.Text = OU;
             }
 
         }
